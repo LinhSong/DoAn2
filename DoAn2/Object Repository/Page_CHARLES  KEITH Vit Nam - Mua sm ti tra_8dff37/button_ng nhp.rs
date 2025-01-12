782faff731b614ac425916cd608045fe9586f8f7
LinhSong <linhsong1909@gmail.com>
2025-01-12 16:55:14 +0700

    Katalon

---
+++ DoAn2/Object Repository/Page_CHARLES  KEITH Vit Nam - Mua sm ti tra_8dff37/button_ng nhp.rs
@@ -0,0 +1,134 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>button_ng nhp</name>
+   <tag></tag>
+   <elementGuidId>a3b9e91f-3ceb-49bd-aaf2-1d46dc1a56a8</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>button.btn.btn-block.btn-primary.btn-primary--hover.header_login-sign_in</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//button[@type='submit']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:role=button[name=&quot;Đăng nhập&quot;i]</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>button</value>
+      <webElementGuid>96f33075-d8cd-4863-b075-7d65f4112b67</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>type</name>
+      <type>Main</type>
+      <value>submit</value>
+      <webElementGuid>5e80e85d-97a0-4433-bb51-7d30718c596f</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>btn btn-block btn-primary btn-primary--hover header_login-sign_in</value>
+      <webElementGuid>08e90535-e935-47cc-925e-7bcced09c0cc</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+            
+                Đăng nhập
+            
+        </value>
+      <webElementGuid>24cabb0c-107b-4506-971d-d4cdc1b14bdf</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/div[@class=&quot;page-content&quot;]/div[@class=&quot;js-page-data page page--has_header  page--home&quot;]/header[@class=&quot;page_header page_header--sticky&quot;]/div[@class=&quot;container-fluid container-fluid-md container-hd page_header-inner  js-logo_wrapper&quot;]/div[@class=&quot;page_header-item page_header-txt page_header-login&quot;]/div[@class=&quot;header_login user dropdown position-static show&quot;]/div[@class=&quot;js-header_login-menu--unregistered dropdown-menu header_login-menu header_login-menu--unregistered show&quot;]/div[@class=&quot;header_login-menu_content&quot;]/form[@class=&quot;js-login_form login_form js-chain_inline_validation_form&quot;]/button[@class=&quot;btn btn-block btn-primary btn-primary--hover header_login-sign_in&quot;]</value>
+      <webElementGuid>c1416a7f-12fe-41ea-93a5-1a493f7f3810</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//button[@type='submit']</value>
+      <webElementGuid>87ce12c8-0f1b-4311-82f0-1eb136286840</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Trường này là bắt buộc'])[2]/following::button[1]</value>
+      <webElementGuid>41dc2a97-ab0b-4e2b-95c8-84d1bd252b36</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Mật khẩu'])[1]/following::button[1]</value>
+      <webElementGuid>c1cdf7e1-409a-450c-b678-45150b47a07b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Ghi nhớ tôi'])[1]/preceding::button[1]</value>
+      <webElementGuid>f5cf0717-2733-4dd6-9741-552e5ee50bdc</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Quên mật khẩu?'])[1]/preceding::button[1]</value>
+      <webElementGuid>bf6c62e9-23a1-4bea-ac79-7056eb281180</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//form/button</value>
+      <webElementGuid>f0f07945-844b-4f74-9592-e1cf5660cec0</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//button[@type = 'submit' and (text() = '
+            
+                Đăng nhập
+            
+        ' or . = '
+            
+                Đăng nhập
+            
+        ')]</value>
+      <webElementGuid>b7fd2b8e-129f-46ce-bebd-93bcdf6105e8</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ DoAn2/Test Suites/Login.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>ecfd0965-80f6-4ca8-88cf-c849982f4aff</testSuiteGuid>
+   <testCaseLink>
+      <guid>bc2fbfe2-1b72-4674-bb37-b0e7560f12b5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>79d904b4-0aac-4eec-ac49-ad263b6c250a</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Data_test</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>79d904b4-0aac-4eec-ac49-ad263b6c250a</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Usename</value>
+         <variableId>450e968f-f843-4223-b894-ee8c643c9b00</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>79d904b4-0aac-4eec-ac49-ad263b6c250a</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Pass</value>
+         <variableId>721e655a-8b92-42a1-8bce-aa2a4986b5ff</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
